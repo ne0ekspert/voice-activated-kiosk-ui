4d6edeb79205f9a12b05ab7491ee35b01b77cbd2
ne0ekspert <rlaaudwk5503@gmail.com>
2024-04-17 13:10:10 +0900

    added cart on order page

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,36 +1,51 @@
 import { Link } from "react-router-dom";
 
-const products = [
-    {name: '신라면', price: 1500, image: null},
-    {name: '진라면', price: 99999, image: null},
-    {name: '육개장', price: 99999, image: null}
-]
-
 function Item({ item, addItem }) {
     return (
-        <div className="shadow-sm border rounded-md flex flex-col h-1/4">
-            <img alt="product"/>
+        <div className="border border-black rounded-md p-6 flex flex-col h-1/4 justify-center" onClick={() => addItem({name: item.name, count: 1})}>
+            <img src={item.image} alt="product"/>
             <div className="flex flex-col">
                 <span>{item.name}</span>
                 <span>{item.price}원</span>
             </div>
             <div className="grow"></div>
-            <button onClick={addItem} className='bg-red-500 text-white'>추가하기</button>
         </div>
-    )
+    );
+}
+
+function CartItem({ item, products }) {
+    const product = products.filter((v) => item.name === v.name)[0];
+
+    return (
+        <div className="w-56 h-56 overflow-x-auto p-10">
+            <div className="flex flex-col bg-white h-full w-full text-black">
+                <span>{item.name}</span>
+                <span>{item.count}</span>
+                <span>{product.price * item.count}</span>
+            </div>
+        </div>
+    );
 }
 
 function Order(props) {
     return (
         <div className='flex flex-col w-full h-full'>
-            <nav className="w-full flex justify-center bg-slate-600 text-white">
-                <button className="transition mr-10">주문 화면</button>
+            <nav className="w-full flex flex-col justify-center bg-slate-600 text-white">
+                <button className="transition mr-10">탭 1</button>
+                <button className="transition mr-10">탭 2</button>
                 <Link to='/payment'><button>결제하기</button></Link>
             </nav>
-            <div className="h-3/4 grid grid-cols-4 gap-4">
+            <div className="h-3/4 grid grid-cols-4 gap-4 p-8">
                 {
-                    products.map((v, i) => <Item item={v} key={i} addItem={props.addItem} />)
+                    props.products.map((v, i) => <Item item={v} key={i} addItem={props.addItem} />)
                 }
+            </div>
+            <div className="bg-slate-800 text-white h-1/4 flex flex-row">
+                {props.list.length === 0 ?
+                (<span>장바구니에 음식을 추가해주세요</span>)
+                : props.list.map((v, i) => (
+                    <CartItem item={v} key={i} products={props.products} />
+                ))}
             </div>
         </div>
     );
