8de12950baecd290761a395486ba8283353b9b38
ne0ekspert <rlaaudwk5503@gmail.com>
2024-04-17 13:09:30 +0900

    Home text update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,8 @@ function Home() {
                 <div className="w-3/4 overflow-hidden">
                     <img src={logo} alt="logo" className="h-full" />
                 </div>
-                <div className="w-1/4">
-                    터치해서 시작해주세요
+                <div className="w-1/4 h-full text-4xl font-bold flex justify-center align-bottom items-end pb-64">
+                    <span className="bg-yellow-300 rounded-full p-3 pl-7 pr-7">화면을 터치해 주세요</span>
                 </div>
             </div>
         </Link>
