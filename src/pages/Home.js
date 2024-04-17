3e6b1ef792cb45cf79edcdd8a22f6f032f2dddb0
ne0ekspert <rlaaudwk5503@gmail.com>
2024-04-17 13:09:01 +0900

    Home screen update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
+import logo from '../res/logo.png';
 
 function Home() {
     return (
         <Link to="/order">
             <div className="flex w-full h-full">
-                <span>터치해서 시작해주세요</span>
+                <div className="w-3/4 overflow-hidden">
+                    <img src={logo} alt="logo" className="h-full" />
+                </div>
+                <div className="w-1/4">
+                    터치해서 시작해주세요
+                </div>
             </div>
         </Link>
     )
